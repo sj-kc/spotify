ee0c8a5e617d0ef5b693e2916edac070cef1c87d
sungkimcastro <s.kim.castro@gmail.com>
2023-08-10 18:43:00 -0600

    Displaying uploaded songs in library

--- components/Library.tsx
+++ components/Library.tsx
@@ -7,7 +7,15 @@ import { useUser } from '@/hooks/useUser';
 import useAuthModal from '@/hooks/useAuthModal';
 import useUploadModal from '@/hooks/useUploadModal';
 
-const Libary = () => {
+import { Song } from '@/types';
+
+import MediaItem from './MediaItem';
+
+interface LibaryProps {
+  songs: Song[];
+}
+
+const Libary: React.FC<LibaryProps> = ({ songs }) => {
   const authModal = useAuthModal();
   const uploadModal = useUploadModal();
   const { user } = useUser();
@@ -34,7 +42,11 @@ const Libary = () => {
           className='text-neutral-400 cursor-pointer hover:text-white transition'
         />
       </div>
-      <div className='flex flex-col gap-y-2 mt-4 px-3'>List of Songs!</div>
+      <div className='flex flex-col gap-y-2 mt-4 px-3'>
+        {songs.map((item) => (
+          <MediaItem onClick={() => {}} key={item.id} data={item} />
+        ))}
+      </div>
     </div>
   );
 };
